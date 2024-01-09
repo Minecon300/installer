055f33737072b13e951fbb72417ec938a3ec44dd
Minecon300 <120320914+Minecon300@users.noreply.github.com>
2024-01-09 22:03:04 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,45 @@
-print("Test")
+local component = require("component")
+local internet  = component.getPrimary("internet")
+local run       = true
+
+local function download(url)
+	local con = internet.request(url)
+	local buf = ""
+	local data = ""
+	while data ~= nil do
+		data = con.read(math.huge)
+		if data ~= nil then
+			buf = buf .. data
+		end
+	end
+	con.close()
+	return buf
+end
+
+function Install(url)
+  if not component.internet then
+    error("internet component not found")
+  else
+
+end
+
+function CheckandInstall(userstr)
+  if (userstr == "Tetris")
+    Install("https://raw.githubusercontent.com/Minecon300/installer/main/tetris.lua")
+  end
+end
+  
+local userstr = ""
+function keyPressed(event_name, player_uuid, ascii)
+    local c = string.char(ascii)
+    if c=='\n' then
+        print(userstr)
+        CheckandInstall(userstr)
+        userstr = ""
+    else
+        userstr=userstr..c
+    end
+    --stores keys typed by user and prints them as a string when you press enter
+end
+
+event.register("key_down", keyPressed)
