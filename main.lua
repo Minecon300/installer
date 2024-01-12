df8b1e3a3bae3c0428632db9ab92e1d28ff392a1
Minecon300 <120320914+Minecon300@users.noreply.github.com>
2024-01-12 22:06:57 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,1 @@
-local component     = require("component")
-local bit32         = require("bit32")
-local internet      = component.getPrimary("internet")
-local term          = require("term")
-local gpu           = term.gpu()
-local event         = require("event")
-local filesystem    = require("filesystem")
-local width, height = gpu.getResolution()
-
-
+print(test?)
